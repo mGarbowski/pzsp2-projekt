cba074db97090479a11f86cdede2229d171ff6b2
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-25 13:32:20 +0100

    fire a request to the backend app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     message: str
