942f0278c560c766c57829c7903b97f65dc237e2
Maksym <bienkowski.maksym@gmail.com>
2024-12-01 21:45:55 +0100

    add frontend part of pyomo demo, fix backend response format

--- backend/src/pzsp_backend/app.py
+++ backend/src/pzsp_backend/app.py
@@ -54,4 +54,5 @@ async def websocket_endpoint(websocket: WebSocket):
 
 @app.post("/integer-model-demo")
 def integer_model_demo(pp: ModelParams):
-    return {"result": solve_instance(pp)}
+    x, y = solve_instance(pp)
+    return {"x": x, "y": y}
