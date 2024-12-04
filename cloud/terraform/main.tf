bcbbab03d955ff03b766f26bbedc9d356c05bcd5
Michał Łuszczek <michluszcz@gmail.com>
2024-12-04 18:31:22 +0100

    change vm public ip allocation to static

--- cloud/terraform/main.tf
+++ cloud/terraform/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_public_ip" "lab2_ip" {
   name                = "lab2-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_virtual_network" "lab2_vnet" {
