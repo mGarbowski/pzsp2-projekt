92cd686c91b84f351f2870a6fd12a0683755c334
mgarbowski <mikolaj.garbowski@gmail.com>
2024-12-04 15:53:34 +0100

    refactor optimizer

--- frontend/src/Optimizer/Optimizer.tsx
+++ frontend/src/Optimizer/Optimizer.tsx
@@ -1,38 +1,16 @@
 import {useState} from "react";
-import useWebSocket from "react-use-websocket";
+import {useOptimizer} from "./useOptimizer.ts";
 
 export const Optimizer = () => {
   const backendBaseUrl = import.meta.env.VITE_BACKEND_URL as string;
   const url = `${backendBaseUrl}/ws/optimizer`;
-
   const [message, setMessage] = useState('');
-  const [lastMessage, setLastMessage] = useState<string | null>(null);
-  const [socketUrl, setSocketUrl] = useState<string | null>(null);
-
-  const {sendMessage, readyState} = useWebSocket(socketUrl, {
-    onOpen: () => console.log('WebSocket connection opened.'),
-    onClose: () => console.log('WebSocket connection closed.'),
-    onMessage: (message) => {
-      const msg = message.data as string;
-      setLastMessage(msg);
-      if (msg.startsWith("Finished")) {
-        setSocketUrl(null);
-      }
-
-    },
-    shouldReconnect: () => false,
-  });
-
-  const handleClick = () => {
-    setSocketUrl(url);
-    sendMessage(message);
-  }
+  const {sendQuery, lastMessage} = useOptimizer(url);
 
   return <div>
     <input type="text" placeholder="Enter your message"
       onChange={(e) => setMessage(e.target.value)}/>
-    <button onClick={handleClick} >Optimize</button>
+    <button onClick={() => sendQuery(message)}>Optimize</button>
     <p>Message: {lastMessage}</p>
-    <p>readyState: {readyState}</p>
   </div>
 }
---
+++ frontend/src/Optimizer/useOptimizer.ts
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import useWebSocket from "react-use-websocket";
+
+export const useOptimizer = (apiUrl: string) => {
+  const [lastMessage, setLastMessage] = useState<string | null>(null);
+  const [socketUrl, setSocketUrl] = useState<string | null>(null);
+
+  const handleOpen = () => console.log('WebSocket connection opened.');
+
+  const handleClose = () => console.log('WebSocket connection closed.');
+
+  const handleMessage = (message: { data: string; }) => {
+    const msg = message.data as string;
+    setLastMessage(msg);
+
+    if (msg.startsWith("Finished")) {
+      setSocketUrl(null);
+    }
+  }
+
+  const {sendMessage} = useWebSocket(socketUrl, {
+    onOpen: handleOpen,
+    onClose: handleClose,
+    onMessage: handleMessage,
+    shouldReconnect: () => false,
+  });
+
+  const sendQuery = (message: string) => {
+    setSocketUrl(apiUrl);
+    sendMessage(message);
+  }
+
+  return {sendQuery, lastMessage}
+}
