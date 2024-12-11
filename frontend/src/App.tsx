f72914f844ec1cc8a79d851be18031d0aae53a23
Maksym <bienkowski.maksym@gmail.com>
2024-12-11 21:32:52 +0100

    remove app.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { WelcomePage } from "./WelcomePage.tsx";
 import { OptimizerPage } from "./Optimizer/OptimizerPage.tsx";
