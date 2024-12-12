155c11b0c8f8ad8223160c314829f67707d3da66
mgarbowski <mikolaj.garbowski@gmail.com>
2024-12-12 18:01:46 +0100

    add delay to Find Channel button

--- frontend/src/Presentation/PresentationPage.tsx
+++ frontend/src/Presentation/PresentationPage.tsx
@@ -5,13 +5,14 @@ import {useNetwork} from "../NetworkModel/NetworkContext.tsx";
 export const PresentationPage = () => {
   const {network, setHighlightedChannelId} = useNetwork();
 
-  const onDummyGenerateChannel = () => {
+  const onFindChannel = () => {
     if (!network) {
       return;
     }
-
-    const firstChannel = Object.keys(network.channels)[0];
-    setHighlightedChannelId(firstChannel);
+    setTimeout(() => {
+      const firstChannel = Object.keys(network.channels)[0];
+      setHighlightedChannelId(firstChannel);
+    }, 3000);
   }
 
   return (
@@ -19,7 +20,7 @@ export const PresentationPage = () => {
       <h1>Presentation</h1>
       <div style={{position: 'fixed', top: "20%", left: "5%", width: '45%', height: '100%', overflow: 'auto'}}>
         <DownloadReport/>
-        <button onClick={onDummyGenerateChannel}>Find channel</button>
+        <button onClick={onFindChannel}>Find channel</button>
       </div>
       <div style={{position: 'fixed', top: "20%", right: "5%", width: '45%', height: '100%', overflow: 'auto'}}>
         <GraphVisualisationDemo/>
