651d1406ad0d939638818463ada001abd2c69898
Maksym <bienkowski.maksym@gmail.com>
2024-12-13 10:43:35 +0100

    style card and navigation menu

--- frontend/src/Optimizer/OptimizerPage.tsx
+++ frontend/src/Optimizer/OptimizerPage.tsx
@@ -1,10 +1,10 @@
 import { OptimizerForm } from "./OptimizerForm";
-import { MainContainer } from "../StyledComponents/maincontainer";
+import { MainContainer } from "../StyledComponents/MainContainer";
 
 export const OptimizerPage = () => {
 
   return <MainContainer>
-    <div className="mb-64">
+    <div className="mb-64 w-7/12">
       <h1 className="text-3xl font-bold mb-16">Dodaj kanał</h1>
       <OptimizerForm />
     </div>
