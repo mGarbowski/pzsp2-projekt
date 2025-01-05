6a403d19ea02ba919e0650125a91ff573e63727d
Maksym <bienkowski.maksym@gmail.com>
2025-01-05 20:49:11 +0100

    map bandwidth to number of slices necessary to accomodate it

--- backend/src/pzsp_backend/optimization/base.py
+++ backend/src/pzsp_backend/optimization/base.py
@@ -20,4 +20,16 @@ class Optimizer(ABC):
 
     @abstractmethod
     def calculate_edge_weight(self, e: Edge) -> float:
-        """Calculates the weights of an edge based on the optimizer's params"""
+        """Calculate the weights of an edge based on the optimizer's params"""
+
+    @staticmethod
+    def bandwidth_from_string(s: str) -> int:
+        """Remove the unit from the bandwidth string and parses it as an int"""
+        return int(s[:-4])
+
+    def num_slices_from_bandwidth(self, bandwidth: str) -> int:
+        """Calculate the minimum number of slices needed to accomodate a given bandwidth"""
+        # TODO: more realistic mapping needed here
+        mapping = {10: 1, 40: 4, 100: 10, 400: 40}
+        bw_as_int = self.bandwidth_from_string(bandwidth)
+        return mapping.get(bw_as_int, 20)  # should actually calculate stuff here
