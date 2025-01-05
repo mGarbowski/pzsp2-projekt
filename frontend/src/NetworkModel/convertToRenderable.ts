d33cb3371d43a3baeb9d0552d962c69fee194827
Michał Łuszczek <michluszcz@gmail.com>
2025-01-05 20:18:52 +0100

    create function for converting imported data to the network.ts format

---
+++ frontend/src/NetworkModel/convertToRenderable.ts
@@ -0,0 +1,56 @@
+import {Node, Channel, Edge, Network} from "./network.ts";
+import {ImportedNetwork} from "../DataImporter/buildNetwork.ts";
+
+export const convertToRenderable = (importedNetwork: ImportedNetwork): Network => {
+  const {nodes, edges, channels} = importedNetwork;
+  const renderableNodes: Record<string, Node> = {};
+  const renderableEdges: Record<string, Edge> = {};
+  const renderableChannels: Record<string, Channel> = {};
+
+  const getEdges = (nodeIds: string[]): Edge[] => {
+    let collectedEdges: Edge[] = []
+
+    for (let i= 0; i < nodeIds.length - 1; i++){
+      let firstNodeId = nodeIds[i]
+      let secondNodeId = nodeIds[i+1]
+      let edge = edges.find((edge) => {edge.node1Id == firstNodeId && edge.node2Id == secondNodeId })
+      if (edge){
+        collectedEdges.push(edge)
+      } else {
+        throw new Error(`Invalid node array. Nodes ${firstNodeId} and ${secondNodeId} don't belong to any edge. `)
+      }
+    }
+
+    return collectedEdges
+  }
+  nodes.forEach((node) => {
+    renderableNodes[node.id] = {
+      id: node.id,
+      latitude: node.latitude,
+      longitude: node.longitude,
+      neighbors: node.neighbors.map((neighbor) => neighbor.node.id),
+    };
+  });
+
+  edges.forEach((edge) => {
+    renderableEdges[edge.id] = {
+      id: edge.id,
+      node1Id: edge.node1Id,
+      node2Id: edge.node2Id,
+      totalCapacity: edge.totalCapacity,
+      provisionedCapacity: edge.provisionedCapacity
+    };
+  });
+
+  channels.forEach((channel) => {
+    renderableChannels[channel.id] = {
+      id: channel.id,
+      nodes: channel.nodes,
+      edges: getEdges(channel.nodes).map((edge) => edge.id),
+      frequency: channel.frequency,
+      width: channel.width
+    };
+  });
+
+  return {nodes: renderableNodes, edges: renderableEdges, channels: renderableChannels};
+};
