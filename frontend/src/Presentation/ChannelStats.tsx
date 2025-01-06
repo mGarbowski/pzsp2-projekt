0dc4710dd282f3ad1fd6b023abe21373c5f61f95
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-06 20:17:54 +0100

    make ids of nodes and edges navigatable in channel stats

--- frontend/src/Presentation/ChannelStats.tsx
+++ frontend/src/Presentation/ChannelStats.tsx
@@ -1,24 +1,28 @@
 import {Card, CardContent, CardHeader, CardTitle} from "../Components/UI/card.tsx";
 import {useNetwork} from "../NetworkModel/NetworkContext.tsx";
 import {calcNodeDistance} from "../NetworkModel/calcNodeDistance.ts";
+import styled from "@emotion/styled";
+import {Channel, Network} from "../NetworkModel/network.ts";
 
+
+const totalChannelLength = (network: Network, channel: Channel) => {
+  const totalLength = channel.edges
+    .map((edgeId) => {
+      const edge = network!.edges[edgeId];
+      return calcNodeDistance(network!.nodes[edge.node1Id], network!.nodes[edge.node2Id]);
+    })
+    .reduce((acc, length) => acc + length, 0);
+  return Math.round(totalLength);
+}
 
 export const ChannelStats = () => {
-  const {network, selectedChannelId} = useNetwork();
+  const {network, selectedChannelId, setSelectedNodeId, setSelectedEdgeId} = useNetwork();
   if (!network || !selectedChannelId) {
     return null;
   }
 
   const channel = network.channels[selectedChannelId];
-
-  const edges = Object.values(channel.edges).join(", ");
-  const nodes = Object.values(channel.nodes).join(", ");
-  const edgeCount = Object.values(channel.edges).length ?? 0;
-  const totalLength = channel.edges.map((edgeId) => {
-    const edge = network!.edges[edgeId];
-    return calcNodeDistance(network!.nodes[edge.node1Id], network!.nodes[edge.node2Id]);
-  }).reduce((acc, length) => acc + length, 0);
-
+  const edgeCount = channel.edges.length ?? 0;
 
   return <Card>
     <CardHeader>
@@ -27,11 +31,29 @@ export const ChannelStats = () => {
     <CardContent>
       <p>ID: {channel.id}</p>
       <p>Liczba krawędzi: {edgeCount}</p>
-      <p>Długość: {Math.round(totalLength)} km</p>
-      <p>Węzły: {nodes}</p>
-      <p>Krawędzie: {edges}</p>
+      <p>Długość: {totalChannelLength(network, channel)} km</p>
       <p>Częstotliwość środkowa: {channel.frequency} THz</p>
       <p>Szerokość pasma: {channel.width} GHz</p>
+      <p>Węzły:</p>
+      <List>
+        {channel.nodes.map((nodeId) => (
+          <li key={nodeId}>
+            <button onClick={() => setSelectedNodeId(nodeId)}>{nodeId}</button>
+          </li>
+        ))}
+      </List>
+      <p>Krawędzie:</p>
+      <List>
+        {channel.edges.map((edgeId) => (
+          <li key={edgeId}>
+            <button onClick={() => setSelectedEdgeId(edgeId)}>{edgeId}</button>
+          </li>
+        ))}
+      </List>
     </CardContent>
   </Card>
 }
+
+const List = styled.ul({
+  marginLeft: "1rem"
+})
