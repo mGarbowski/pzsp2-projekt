9f41429b53a3f12fd48707eadb470d5a19a37305
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-06 20:08:45 +0100

    make list of node neighbors naviagatable

--- frontend/src/Presentation/NodeStats.tsx
+++ frontend/src/Presentation/NodeStats.tsx
@@ -1,16 +1,15 @@
 import {Card, CardContent, CardHeader, CardTitle} from "../Components/UI/card.tsx";
 import {useNetwork} from "../NetworkModel/NetworkContext.tsx";
+import styled from "@emotion/styled";
 
 
 export const NodeStats = () => {
-  const {network, selectedNodeId} = useNetwork();
+  const {network, selectedNodeId, setSelectedNodeId} = useNetwork();
   if (!network || !selectedNodeId) {
     return null;
   }
 
   const node = network.nodes[selectedNodeId];
-
-  const neighbors = node.neighbors.join(", ");
 
   return <Card>
     <CardHeader>
@@ -21,7 +20,18 @@ export const NodeStats = () => {
       <p>Szerokość: {node.latitude}</p>
       <p>Długość: {node.longitude}</p>
       <p>Stopień: {node.neighbors.length}</p>
-      <p>Sąsiedzi: {neighbors}</p>
+      <p>Sąsiedzi:</p>
+      <List>
+        {node.neighbors.map((neighbor) => (
+          <li key={neighbor}>
+            <button onClick={() => setSelectedNodeId(neighbor)}>{neighbor}</button>
+          </li>
+        ))}
+      </List>
     </CardContent>
   </Card>
 }
+
+const List = styled.ul({
+  marginLeft: "1rem"
+})
