bd0fdc8758a465b86df19ed4d090215ad725aa7f
Michał Łuszczek <michluszcz@gmail.com>
2025-01-06 16:22:57 +0100

    catch error if useNetwork fails

--- frontend/src/ReportGeneration/DownloadReport.tsx
+++ frontend/src/ReportGeneration/DownloadReport.tsx
@@ -3,14 +3,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileExport } from '@fortawesome/free-solid-svg-icons'
 import { Button } from "../Components/UI/button.tsx";
 import {useNetwork} from "../NetworkModel/NetworkContext.tsx";
+import {Network} from "../NetworkModel/network.ts";
 export const DownloadReport = () => {
 
   const handleDownload = () => {
-    const {network} = useNetwork()
-    if (!network){
-      return
+    let networkState: Network
+    try {
+      const {network} = useNetwork()
+      if (!network){
+        return;
+      }
+      networkState = network
+    } catch (e){
+      console.error(e)
+      console.log("cannot use useNetwork here")
+      return;
     }
-    const report = generateChannelsReport(Object.values(network.channels));
+
+    const report = generateChannelsReport(Object.values(networkState.channels));
     const blob = new Blob([report], { type: 'text/csv' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
