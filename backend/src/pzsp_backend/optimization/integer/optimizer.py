9b0f4b5a10002126497f1388851f7bdb2ab7460d
Maksym <bienkowski.maksym@gmail.com>
2025-01-07 17:46:06 +0100

    remove double definition of edge slice occupancy map that shadowed the correct one

--- backend/src/pzsp_backend/optimization/integer/optimizer.py
+++ backend/src/pzsp_backend/optimization/integer/optimizer.py
@@ -136,7 +136,8 @@ class IntegerProgrammingOptimizer(Optimizer):
 
     def edge_slice_occupancy_map(self) -> dict[tuple[str, str, int], Literal[0, 1]]:
         """Create a dictionary (node_1_id, node_2_id, slice_idx): slice_occupancy
-        where slice_occupancy is binary (0 - free, 1 - occupied)."""
+        where slice_occupancy is binary (0 - free, 1 - occupied) in both edge directions.
+        """
         # Initialize the dictionary with all combinations defaulting to 0
         rv: dict[tuple[str, str, int], Literal[0, 1]] = {}
         slice_indices = range(768)  # Slice indices from 0 to 767
@@ -145,6 +146,7 @@ class IntegerProgrammingOptimizer(Optimizer):
         for _, edge in self.network.edges.items():
             for slice_idx in slice_indices:
                 rv[(edge.node1Id, edge.node2Id, slice_idx)] = 0
+                rv[(edge.node2Id, edge.node1Id, slice_idx)] = 0
 
         # Update the dictionary with occupied slices
         for _, ch in self.network.channels.items():
@@ -155,5 +157,6 @@ class IntegerProgrammingOptimizer(Optimizer):
             for edge in edges:
                 for slice_idx in occupied_slice_indices:
                     rv[(edge.node1Id, edge.node2Id, slice_idx)] = 1
+                    rv[(edge.node2Id, edge.node1Id, slice_idx)] = 1
 
         return rv
