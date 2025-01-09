199606f8043243c955e062d5b127f2e6bf48d7d7
Maksym <bienkowski.maksym@gmail.com>
2025-01-09 19:41:35 +0100

    test nonexisting optimizer response

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from src.pzsp_backend.optimization.constants import FAILURE, SUCCESS
+from src.pzsp_backend.optimization.constants import FAILURE, INVALID_REQUEST, SUCCESS
 from src.pzsp_backend.app import app
 from src.pzsp_backend.models import Channel, OptimisationRequest, OptimisationResponse
 from tests.util import test_network
@@ -47,3 +47,23 @@ def test_optimizer_endpoint_valid_request(monkeypatch, test_network):
             id="C1", nodes=["N1", "N2"], edges=["E1"], frequency=193.1, width=12.5
         )
         assert response.message == "Optimizer found a solution"
+
+
+def test_optimizer_endpoint_ask_for_nonexisting_optimizer(test_network):
+    request = OptimisationRequest(
+        network=test_network,
+        source="N1",
+        target="N2",
+        bandwidth="10GB/s",
+        optimizer="nonexisting",
+        distanceWeight=1,
+        evenLoadWeight=1,
+    )
+
+    with client.websocket_connect("/ws/optimizer") as ws:
+        ws.send_json(OptimisationRequest.model_dump(request))
+        response = ws.receive_json()
+        response = OptimisationResponse.model_validate_json(response)
+        assert response.type == INVALID_REQUEST
+        assert response.channel is None
+        assert response.message == "Invalid optimizer requested: nonexisting"
